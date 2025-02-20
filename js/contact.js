e18e96f1d08ae9b8ee141aba4599614d296fd915
voan24ge <voan24ge@student.ju.se>
2025-02-20 13:54:14 +0100

    a new return button is added and styled

--- js/contact.js
+++ js/contact.js
@@ -5,15 +5,26 @@ const mainElement = document.querySelector("main");
 buttonElement.addEventListener("click", submition);
 
 function submition() {
+  // clear previous content
+  while (mainElement.firstChild) {
+    mainElement.removeChild(mainElement.firstChild);
+  }
+  // create success message
   const paragraphElement = document.createElement("p");
   paragraphElement.innerText = "Your message is successfully sent!";
   paragraphElement.classList.add("submit-style");
 
-  mainElement.appendChild(paragraphElement);
+  // create return button
+  const newButton = document.createElement("button");
+  newButton.innerText = "Return to the previous page";
+  newButton.classList.add("return-button-style");
 
-  while (mainElement.firstChild) {
-    mainElement.removeChild(mainElement.firstChild);
-  }
+  newButton.addEventListener("click", function (event) {
+    // the next function was suggested by ChatGPT: https://chatgpt.com/c/67b72486-4d5c-800b-a2b5-b233461c140c
+    window.location.href = "contact.html";
+  });
 
+  // append elements to main
   mainElement.appendChild(paragraphElement);
+  mainElement.appendChild(newButton);
 }
